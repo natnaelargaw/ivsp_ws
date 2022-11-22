e509ba51677af7285d6f41f6fb591ba13db87619
natnaelargaw <natnaelargaw@gmail.com>
2022-11-22 15:18:36 +0100

    first draft ivsp push

--- ivsp/src/video_saliency.py
+++ ivsp/src/video_saliency.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+from __future__ import division
 from __future__ import print_function
 
 import roslib
@@ -9,6 +10,22 @@ import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from collections import deque
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+
+from tensorflow.keras.optimizers import Adam
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
+import os
+import numpy as np
+from config import *
+from model import *
+from utility import *
+
+from PIL import Image as im
+from numpy import asarray
+
 
 """
 This class subscribes to any live video stream, /naoqi_driver/camera/front/image_raw topic in this case, and 
@@ -31,52 +48,75 @@ class dynamic_saliency_prediction:
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/naoqi_driver/camera/front/image_raw", Image, self.saliency_model_callback)
 
-  
+    self.x = Input(batch_shape=(1, None, shape_r, shape_c, 3))
+    self.x2 = Input(batch_shape=(1, None, shape_r, shape_c, 3))
+    self.x3 = Input(batch_shape=(1, None, shape_r, shape_c, 3))
+    self.stateful = True
+    self.m = Model(inputs=[self.x, self.x2, self.x3], outputs=transform_saliency([self.x, self.x2, self.x3], self.stateful))
+    print("Loading MODS weights")
+    self.m.load_weights('Saliencyconvlstm.h5') 
+    self.queue = deque()
+
+
   """ callback function for the topic /naoqi_driver/camera/front/image_raw  """
   def saliency_model_callback(self,data):
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+   
+    # get video access
+    if len(self.queue) < num_frames:
+      self.queue.append(cv_image)
+    else:
+      self.queue.popleft()
+      self.queue.append(cv_image)
 
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, ((cols/2)-15,(rows/2)-15), 30, 255)
+      Xims = np.zeros((1, len(self.queue), shape_r, shape_c, 3))  # change dimensionality
+      Xims2 = np.zeros((1, len(self.queue), shape_r, shape_c, 3))  # change dimensionality
+      Xims3 = np.zeros((1, len(self.queue), shape_r, shape_c, 3))  # change dimensionality
 
-      cv2.imshow("Image window", cv_image)
-      cv2.waitKey(3)
-      """ prediction and locomotion command is acquired after this function call """
-      saliency_map = self.predict(cv_image)
+      [X, X2, X3] = preprocess_images_realtime(self.queue, shape_r, shape_c)
+      # print(X.shape, "X shape new")
+      #   # print("Inside Test Generator ", X.shape)
+      Xims[0] = np.copy(X)
+      Xims2[0] = np.copy(X2)
+      Xims3[0] = np.copy(X3)
 
-      [X, Y, Z] = self.localize_torsow(saliency_map)
+      prediction = self.m.predict([Xims,Xims2,Xims3])
+      # print("Prediction shape: ", prediction.shape)
 
-      
+      for j in range(len(self.queue)):
+        orignal_image = self.queue[0]
+        print(orignal_image.shape, "Queue shape")
+        x, y = divmod(j, len(self.queue))
+
+      #print(prediction[0,0,:,:,0].shape)
+      self.publish_prediction(prediction[0,0,:,:,0])
+      self.m.reset_states()
   
-  """ Video saliency prediction model """
-  def predict(self, image_data):
+  """ Moving object detection and segmentation model """
+  def publish_prediction(self, image_data):
     """
-    load model here  
-    predict
+    publish mods map here
     """
+    # print("Publishable data received")
     predicted_map = image_data
     try:
-        self.saliency_map_pub.publish(self.bridge.cv2_to_imgmsg(predicted_map, "bgr8"))
-        print(predicted_map.shape)
+        self.saliency_map_pub.publish(self.bridge.cv2_to_imgmsg(predicted_map, "32FC1"))
+        print(predicted_map.shape, " MODS map published")
     except CvBridgeError as e:
         print(e)
 
-    return predicted_map
-
+    # localize_torsow(predicted_map)
 
   """ calculates x,y,z value of all dof for the robot torsow and publishes both to the
-   torsow subscriber and a separate topic called /saliency/salient_roi 
+   torsow subscriber and a separate topic called /mods/object_roi 
   """
-  def localize_torsow(self, saliency_map):
+  # robot head movement setter. under development
+  def localize_torsow(self, object_map):
     hello_coordinte = "hello [X, Y, Z]%s   this is the coordinate" % rospy.get_time()
     self.localize_pub.publish(hello_coordinte)
-
-    return ["X", "Y", "Z"]
-
 
 """ entry function for the video saliency prediction model"""
 def main(args):
@@ -86,7 +126,7 @@ def main(args):
     rospy.spin()
   except KeyboardInterrupt:
     print("Shutting down")
-  cv2.destroyAllWindows()
+  # cv2.destroyAllWindows()
 
 """ entry point for the file """
 if __name__ == '__main__':
